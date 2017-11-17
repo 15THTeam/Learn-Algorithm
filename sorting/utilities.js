301d0e8de581b7fd243e268c460db5ab2ae3668a
Viet Anh <vietanhtong1508@gmail.com>
2017-11-17 15:15:06 +0700

    Add bubble-sort

--- sorting/utilities.js
+++ sorting/utilities.js
@@ -1,18 +1,13 @@
-var gen = (length, rand) => {
-    var array = [];
-    for (i = 0; i < length; i++) {
+let gen = (length, rand) => {
+    let array = [];
+    for (i = 0; i < length; ++i) {
         array.push(Math.floor(Math.random() * rand));
     }
     return array;
 }
 
-var log = array => {
-    var result = '';
+let log = array => {
+    let result = '';
     array.forEach(item => result += item + ' ');
     console.log(result);
-}
-
-module.exports = {
-    gen,
-    log
 }
